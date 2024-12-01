6128b1651475977b0a9d5afe4e591fd8b83efc84
Mike Neilens <michaelneilens@Michaels-Mac-mini.local>
2024-12-01 09:33:06 +0000

    advent day1

--- src/main/kotlin/day01/Problem.kt
+++ src/main/kotlin/day01/Problem.kt
@@ -1,9 +1,31 @@
 package day01
 
-fun partOne(data:List<String>): Int {
-    return 0
-}
+import kotlin.math.abs
+
+fun partOne(data:List<String>) = parse(data).matchData().sum()
+
+fun parse(data:List<String>) = Pair(
+    data.map{it.split("   ")[0].toInt()}.sorted(),
+    data.map{it.split("   ")[1].toInt()}.sorted()
+    )
+
+fun Pair<List<Int>,List<Int>>.matchData(result:List<Int> = listOf()):List<Int> =
+    if (first.isEmpty()) result
+    else Pair(first.drop(1), second.drop(1)).matchData( result + abs(first.first() - second.first()))
 
 fun partTwo(data:List<String>): Int {
-    return 0
+    val (list1, list2) = parse(data)
+    return createSimilarityScore(list1, list2).sum()
 }
+
+fun createSimilarityScore(list1: List<Int>, list2: List<Int>): List<Int> {
+    val map = createMapOfOccurrence(list1, list2)
+    return list1.map{ it * map.getValue(it) }
+}
+
+fun createMapOfOccurrence(list1:List<Int>, list2:List<Int>, map:MutableMap<Int, Int> = mutableMapOf()):Map<Int, Int> =
+    if (list1.isEmpty() ) map
+    else createMapOfOccurrence(list1.drop(1), list2, map).also {
+        if (list1.first() !in map) map[list1.first()] = list1.first().occurrenceIn(list2) }
+
+fun Int.occurrenceIn(list:List<Int>) = list.count { it == this}
--- src/test/kotlin/day01/ProblemTest.kt
+++ src/test/kotlin/day01/ProblemTest.kt
@@ -5,17 +5,44 @@ import io.kotest.matchers.shouldBe
 
 class ProblemTest:WordSpec ({
     val sampleData = """
-        
+        3   4
+        4   3
+        2   5
+        1   3
+        3   9
+        3   3
     """.trimIndent().split("\n")
 
     "With part one" should {
-        "return 0" {
-            partOne(sampleData) shouldBe 0
+        "parse input into two lists" {
+            parse(sampleData) shouldBe Pair(
+                listOf(1,2,3,3,3,4), listOf(3,3,3,4,5,9)
+            )
+        }
+        "matching sample data should give [2,1,0,1,2,5]" {
+            parse(sampleData).matchData() shouldBe listOf(2,1,0,1,2,5)
+        }
+        "with part one test data the answer should be 11" {
+            partOne(sampleData) shouldBe 11
+        }
+        "answer to part one is 1834060" {
+            partOne(puzzleInput) shouldBe 1834060
         }
     }
     "With part two" should {
-        "return 0" {
-            partTwo(sampleData) shouldBe 0
+        "4 appears in second list 1 times" {
+            val (_, list2) = parse(sampleData)
+            4.occurrenceIn(list2) shouldBe 1
+        }
+        "similarity score of each number in list 1 in list 2 is [3,1,0,0,3,3]" {
+            val (list1, list2) = parse(sampleData)
+            createSimilarityScore(list1, list2) shouldBe listOf(0, 0, 9, 9, 9, 4)
+        }
+        "with part two test data the answer is 31" {
+            partTwo(sampleData) shouldBe 31
+        }
+        "answer to part two is " {
+            partTwo(puzzleInput) shouldBe 21607792
         }
     }
 })
