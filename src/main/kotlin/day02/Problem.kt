07c22c94fd955361b3acee7fae7681092522c2c7
Mike Neilens <michaelneilens@Michaels-Mac-mini.local>
2024-12-02 14:46:59 +0000

    advent day2

--- src/main/kotlin/day02/Problem.kt
+++ src/main/kotlin/day02/Problem.kt
@@ -10,9 +10,7 @@ fun List<Int>.diffs() = windowed(2,1).map{it[0] - it[1]}
 
 fun List<Int>.areValid() = all{diff -> diff in 1..3} || all{diff -> diff in -3..-1}
 
-fun partTwo(data:List<String>): Int {
-    return data.parse().count { it.reportIsValid() }
-}
+fun partTwo(data:List<String>) = data.parse().count { it.reportIsValid() }
 
 fun List<Int>.reportIsValid() =
     diffs().areValid() || removeOneElement().any{it.diffs().areValid()}
