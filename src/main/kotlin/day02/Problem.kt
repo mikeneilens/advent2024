5fcab703e8101e8e05774dfb416fb17ab1865561
Mike Neilens <michaelneilens@Michaels-Mac-mini.local>
2024-12-02 08:45:43 +0000

    advent day2

--- src/main/kotlin/day02/Problem.kt
+++ src/main/kotlin/day02/Problem.kt
@@ -1,8 +1,16 @@
 package day02
 
-fun partOne(data:List<String>): Int {
-    return 0
-}
+import kotlin.math.abs
+
+fun partOne(data:List<String>) = data.parse().count{it.diffs().areValid()}
+
+fun List<String>.parse() = map(String::parse)
+
+fun String.parse() = split(" ").map(String::toInt)
+
+fun List<Int>.diffs() = windowed(2,1).map{it[0] - it[1]}
+
+fun List<Int>.areValid() = (all{it > 0} || all{it < 0}) && all{abs(it) in 1..3}
 
 fun partTwo(data:List<String>): Int {
     return 0
--- src/test/kotlin/day02/ProblemTest.kt
+++ src/test/kotlin/day02/ProblemTest.kt
@@ -5,12 +5,42 @@ import io.kotest.matchers.shouldBe
 
 class ProblemTest:WordSpec ({
     val sampleData = """
-        
+        7 6 4 2 1
+        1 2 7 8 9
+        9 7 6 2 1
+        1 3 2 4 5
+        8 6 4 4 1
+        1 3 6 7 9
     """.trimIndent().split("\n")
 
     "With part one" should {
-        "return 0" {
-            partOne(sampleData) shouldBe 0
+        "parse sample data correctly" {
+            sampleData.parse() shouldBe listOf(
+                listOf(7,6,4,2,1),
+                listOf(1,2,7,8,9),
+                listOf(9,7,6,2,1),
+                listOf(1,3,2,4,5),
+                listOf(8,6,4,4,1),
+                listOf(1,3,6,7,9)
+            )
+        }
+        "diffs for [7,6,4,2,1] should be [1,2,2,1]" {
+            listOf(7,6,4,2,1).diffs() shouldBe listOf(1,2,2,1)
+        }
+        "[1,2,2,1] is valid" {
+            listOf(1,2,2,1).areValid() shouldBe true
+        }
+        "[-1,-2,-3,-1] is valid" {
+            listOf(-1,-2,-3,-1).areValid() shouldBe true
+        }
+        "[-1,-2,-4,-1] is not valid" {
+            listOf(-1,-2,-4,-1).areValid() shouldBe false
+        }
+        "with sample data 2 reports are valid" {
+            partOne(sampleData) shouldBe 2
+        }
+        "the answer for part one is 663" {
+            partOne(puzzleInput) shouldBe 663
         }
     }
     "With part two" should {
