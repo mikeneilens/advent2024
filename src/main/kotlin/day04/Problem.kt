254d753989e330db6c2d6a96ea4aac9d87bc4f01
Mike Neilens <michaelneilens@Michaels-Mac-mini.local>
2024-12-04 10:18:05 +0000

    advent day4

--- src/main/kotlin/day04/Problem.kt
+++ src/main/kotlin/day04/Problem.kt
@@ -1,9 +1,45 @@
 package day04
 
-fun partOne(data:List<String>): Int {
-    return 0
+
+data class Position(val row: Int, val col: Int) {
+    operator fun plus(other: Position) = Position(this.row + other.row, this.col + other.col)
+    operator fun times(scalar: Int) = Position(this.row * scalar, this.col * scalar)
+
+    fun lines(length: Int) = listOf(lineR(length),lineL(length),lineU(length), lineD(length),
+        lineUL(length), lineUR(length),lineDL(length), lineDR(length),
+        )
+
+    fun lineR(length:Int) = (0 until length).map { Position(this.row, this.col) + Position(0,1) * it }
+    fun lineL(length:Int) = (0 until length).map { Position(this.row, this.col) + Position(0,-1) * it }
+    fun lineU(length:Int) = (0 until length).map { Position(this.row, this.col) + Position(-1,0) * it }
+    fun lineD(length:Int) = (0 until length).map { Position(this.row, this.col) + Position(1,0) * it }
+    fun lineUL(length:Int) = (0 until length).map { Position(this.row, this.col) + Position(-1,-1) * it }
+    fun lineUR(length:Int) = (0 until length).map { Position(this.row, this.col) + Position(-1,1) * it }
+    fun lineDL(length:Int) = (0 until length).map { Position(this.row, this.col) + Position(1,-1) * it }
+    fun lineDR(length:Int) = (0 until length).map { Position(this.row, this.col) + Position(1,1) * it }
+
+    fun masLines(length: Int) = listOf(
+        lineDR(length) + (this + Position(length - 1,0)).lineUR(length  ),
+        lineDR(length) + (this + Position(0,length - 1)).lineDL(length  ),
+        (this + Position(length - 1, length - 1)).lineUL(length  ) + (this + Position(length - 1,0)).lineUR(length  ) ,
+        (this + Position(length - 1, length - 1)).lineUL(length  ) + (this + Position(0,length - 1)).lineDL(length  )
+    )
 }
 
-fun partTwo(data:List<String>): Int {
-    return 0
-}
+fun partOne(data:List<String>) =
+    data.flatMapIndexed{row, line -> line.indices.map{col -> data.wordsAt(Position(row, col))}}.sumOf { it.size }
+
+fun List<String>.wordsAt(position: Position, word:String = "XMAS") =
+    position.lines(word.length).map{line -> word(line)}.filter { it == word }
+
+fun List<String>.word(positions:List<Position>) = positions.map{char(it)}.joinToString("")
+
+fun List<String>.char(position: Position) = if (position.row in indices && position.col in get(position.row).indices) this[position.row][position.col] else ""
+
+fun partTwo(data:List<String>) =
+    data.flatMapIndexed{row, line -> line.indices.map{col -> data.masWordsAt(Position(row, col))}}.sumOf { it.size }
+
+fun List<String>.masWordsAt(position: Position, word:String = "MASMAS") =
+    position.masLines(word.length/2).map{line -> word(line)}.filter { it == word }
+
+
--- src/test/kotlin/day04/ProblemTest.kt
+++ src/test/kotlin/day04/ProblemTest.kt
@@ -5,17 +5,54 @@ import io.kotest.matchers.shouldBe
 
 class ProblemTest:WordSpec ({
     val sampleData = """
-        
+MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX        
     """.trimIndent().split("\n")
 
     "With part one" should {
-        "return 0" {
-            partOne(sampleData) shouldBe 0
+        "line of length 4 to right of (5,4) is (5,4), (5,5), (5,6), (5,7)" {
+            Position(5,4).lineR(4) shouldBe listOf(
+                Position(5,4),
+                Position(5,5),
+                Position(5,6),
+                Position(5,7),
+            )
+        }
+        "words at position(9,9_ in sample are [XMAS,XMAS]" {
+            sampleData.wordsAt(Position(9,9)) shouldBe listOf("XMAS")
+        }
+        "part one using sample data should be 18" {
+            partOne(sampleData) shouldBe 18
+        }
+        "answer to part one is 2543" {
+            partOne(puzzleInput) shouldBe 2543
         }
     }
     "With part two" should {
-        "return 0" {
-            partTwo(sampleData) shouldBe 0
+        "mas lines at position (1,1) are " {
+            Position(1,1).masLines(3) shouldBe listOf(
+                listOf(Position(row=1, col=1), Position(row=2, col=2), Position(row=3, col=3), Position(row=3, col=1), Position(row=2, col=2), Position(row=1, col=3)),
+                listOf(Position(row=1, col=1), Position(row=2, col=2), Position(row=3, col=3), Position(row=1, col=3), Position(row=2, col=2), Position(row=3, col=1)),
+                listOf(Position(row=3, col=3), Position(row=2, col=2), Position(row=1, col=1), Position(row=3, col=1), Position(row=2, col=2), Position(row=1, col=3)),
+                listOf(Position(row=3, col=3), Position(row=2, col=2), Position(row=1, col=1), Position(row=1, col=3), Position(row=2, col=2), Position(row=3, col=1)),
+                )
+        }
+        "words at position(1,5) are (MASMAS)" {
+            sampleData.masWordsAt(Position(1,5)) shouldBe listOf("MASMAS")
+        }
+        "part two using sample data should be 9" {
+            partTwo(sampleData) shouldBe 9
+        }
+        "answer to part two is 1930 " {
+            partTwo(puzzleInput) shouldBe 1930
         }
     }
 })
