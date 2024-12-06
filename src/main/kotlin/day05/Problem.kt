bbfc043c098fefcbcc5c28c5fba48484634eba83
Mike Neilens <michaelneilens@Michaels-Mac-mini.local>
2024-12-06 08:36:08 +0000

    advent day5

--- src/main/kotlin/day05/Problem.kt
+++ src/main/kotlin/day05/Problem.kt
@@ -1,9 +1,33 @@
 package day05
 
-fun partOne(data:List<String>): Int {
-    return 0
+fun partOne(data:List<String>,rulesData:List<String>): Int {
+    val mustBeBefore = mutableMapOf<String, Set<String>>()
+    parseToBeforeMap(rulesData, mustBeBefore)
+    return data.map{it.split(",")}
+        .filter { it  == it.correctOrder(mustBeBefore)}
+        .sumOf { it[it.size/2].toInt() }
 }
 
-fun partTwo(data:List<String>): Int {
-    return 0
+fun parseToBeforeMap(rulesData:List<String>, mustBeBefore:MutableMap<String, Set<String>> = mutableMapOf()):Map<String, Set<String>>{
+    rulesData.forEach{
+        mustBeBefore.addToMap( Pair( it.split("|").first(),it.split("|").last()))
+    }
+    return mustBeBefore
 }
+
+fun MutableMap<String,Set<String>>.addToMap(values: Pair<String, String>) {
+    if (values.first !in this) this[values.first] = setOf(values.second)
+    else this[values.first] = getValue(values.first) + values.second
+}
+
+fun partTwo(data:List<String>,rulesData:List<String>): Int {
+    val mustBeBefore = mutableMapOf<String, Set<String>>()
+    parseToBeforeMap(rulesData, mustBeBefore)
+    return data.map{it.split(",")}
+        .filter { it  != it.correctOrder(mustBeBefore)}
+        .map{it.correctOrder(mustBeBefore)}
+        .sumOf { it[it.size/2].toInt() }
+}
+
+fun List<String>.correctOrder(mustBeBefore:Map<String, Set<String>>) =
+    sortedByDescending{ (this.toSet() intersect mustBeBefore.getOrDefault(it, setOf())).size }
--- src/test/kotlin/day05/ProblemTest.kt
+++ src/test/kotlin/day05/ProblemTest.kt
@@ -4,18 +4,68 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class ProblemTest:WordSpec ({
-    val sampleData = """
-        
+    val sampleRulesData = """
+        47|53
+        97|13
+        97|61
+        97|47
+        75|29
+        61|13
+        75|53
+        29|13
+        97|29
+        53|29
+        61|53
+        97|53
+        61|29
+        47|13
+        75|47
+        97|75
+        47|61
+        75|61
+        47|29
+        75|13
+        53|13
+    """.trimIndent().split("\n")
+    val sampleUpdatesData = """
+        75,47,61,53,29
+        97,61,53,29,13
+        75,29,13
+        75,97,47,61,53
+        61,13,29
+        97,13,75,29,47
     """.trimIndent().split("\n")
 
     "With part one" should {
-        "return 0" {
-            partOne(sampleData) shouldBe 0
+        "parse rules into two maps" {
+            val mustBeBefore = parseToBeforeMap(sampleRulesData)
+            mustBeBefore["47"] shouldBe setOf("53", "13", "61", "29")
+        }
+        "part one with sample data is 143 " {
+            partOne(sampleUpdatesData, sampleRulesData) shouldBe 143
+        }
+        "answer to part one is 4905" {
+            partOne(puzzleInput, puzzleRule) shouldBe 4905
         }
     }
     "With part two" should {
-        "return 0" {
-            partTwo(sampleData) shouldBe 0
+        "75,97,47,61,53 can be fixed to become 97,75,47,61,53" {
+            val mustBeBefore = parseToBeforeMap(sampleRulesData)
+            listOf("75","97","47","61","53").correctOrder(mustBeBefore) shouldBe listOf("97","75","47","61","53")
+        }
+        "61,13,29 can be fixed to become 61,29,13" {
+            val mustBeBefore = parseToBeforeMap(sampleRulesData)
+            listOf("61","13","29").correctOrder(mustBeBefore) shouldBe listOf("61","29","13")
+        }
+        "97,13,75,29,47 can be fixed to become 97,75,47,29,13" {
+            val mustBeBefore = parseToBeforeMap(sampleRulesData)
+            listOf("97","13","75","29","47").correctOrder(mustBeBefore) shouldBe listOf("97","75","47","29","13")
+        }
+        "part two with sample data is 123" {
+            partTwo(sampleUpdatesData, sampleRulesData) shouldBe 123
+        }
+        "answer to part two is " {
+            partTwo(puzzleInput, puzzleRule) shouldBe 6204
         }
     }
 })
