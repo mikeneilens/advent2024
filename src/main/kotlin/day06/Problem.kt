f3456a5b8bb966567fa8b4d1822047d93d44a6c9
Mike Neilens <michaelneilens@Michaels-Mac-mini.local>
2024-12-06 21:24:59 +0000

    advent day5

--- src/main/kotlin/day06/Problem.kt
+++ src/main/kotlin/day06/Problem.kt
@@ -1,9 +1,46 @@
 package day06
 
+import lib.Position
+
 fun partOne(data:List<String>): Int {
-    return 0
+    val start = data.startPostion()
+    val visited = data.move(start, Position.up)
+    return visited.count()
 }
 
-fun partTwo(data:List<String>): Int {
-    return 0
+val nextDirection = mapOf(Position.up to Position.right, Position.right to Position.down, Position.down to Position.left, Position.left to Position.up)
+
+fun List<String>.move(position:Position, direction:Position, visited:MutableSet<Position> = mutableSetOf()): Set<Position> {
+    visited.add(position)
+    val nextSquare = char(position + direction)
+    if (nextSquare.isEmpty()) return visited
+    val newDirection = if ( nextSquare == "#") nextDirection.getValue(direction) else direction
+    return move(position + newDirection, newDirection, visited)
 }
+
+fun List<String>.startPostion(): Position
+        = indexOfFirst { it.contains("^") }.let{row ->
+    Position(row, get(row).indexOfFirst{it == '^'})
+}
+
+fun List<String>.char(position: Position) = if (position.row in indices && position.col in get(position.row).indices) this[position.row][position.col].toString() else ""
+
+fun partTwo(data:List<String>): Int {
+    val start = data.startPostion()
+    val obstacles = data.flatMapIndexed { row, s -> s.indices.map { col -> Position(row, col) }  }
+    return obstacles.count{data.moveIsLoop(start, Position.up, it)}
+}
+
+fun List<String>.moveIsLoop(position:Position, direction:Position, obstruction:Position, visited:MutableSet<Pair<Position,Position>> = mutableSetOf()): Boolean {
+    if(visited.contains(Pair(position,direction))) return true
+    visited.add(Pair(position,direction))
+    if (char(position + direction).isEmpty()) return false
+    val newDirection = newDirection(position, direction, obstruction)
+    return moveIsLoop(position + newDirection, newDirection, obstruction, visited)
+}
+
+fun List<String>.newDirection(position:Position, direction:Position, obstruction:Position):Position =
+    (position + direction).let{newPosition -> if (char(newPosition) == "#" || newPosition == obstruction)
+        newDirection(position, nextDirection.getValue(direction),obstruction)
+    else direction }
+
--- src/test/kotlin/day06/ProblemTest.kt
+++ src/test/kotlin/day06/ProblemTest.kt
@@ -2,20 +2,56 @@ package day06
 
 import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
+import lib.Position
 
 class ProblemTest:WordSpec ({
     val sampleData = """
-        
+....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...        
     """.trimIndent().split("\n")
 
     "With part one" should {
-        "return 0" {
-            partOne(sampleData) shouldBe 0
+        "start position in sample data is " {
+            sampleData.startPostion() shouldBe Position(6, 4)
+        }
+        "part one with sample data is 41" {
+            partOne(sampleData) shouldBe 41
+        }
+        "answer to part one is 5145 " {
+            partOne(puzzleInput) shouldBe 5145
         }
     }
     "With part two" should {
-        "return 0" {
-            partTwo(sampleData) shouldBe 0
+        "moving on sample data with no obstructions should not be a move in a loop" {
+            val startPosition = sampleData.startPostion()
+            sampleData.moveIsLoop(startPosition, Position.up, Position(-2,-2)) shouldBe false
         }
+        "moving on sample data with obstruction at (6,3) should a move in a loop" {
+            val startPosition = sampleData.startPostion()
+            sampleData.moveIsLoop(startPosition, Position.up, Position(6,3)) shouldBe true
+        }
+        "moving on sample data with obstruction at (7,6) should a move in a loop" {
+            val startPosition = sampleData.startPostion()
+            sampleData.moveIsLoop(startPosition, Position.up, Position(7,6)) shouldBe true
+        }
+        "part two with sample data is 6" {
+            partTwo(sampleData) shouldBe 6
+        }
+        "answer to part two is 1523" {
+            //1539 is wrong
+            partTwo(puzzleInput) shouldBe 1523        }
     }
 })
+
+fun List<String>.startPostion():Position
+    = indexOfFirst { it.contains("^") }.let{row ->
+        Position(row, get(row).indexOfFirst{it == '^'})
+    }
