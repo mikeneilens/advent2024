5956daf31637c0eaa58a0df85b04752ad3f9b7f4
Mike Neilens <michaelneilens@Michaels-Mac-mini.local>
2024-12-10 22:54:05 +0000

    advent day10

--- src/main/kotlin/lib/Position.kt
+++ src/main/kotlin/lib/Position.kt
@@ -29,6 +29,10 @@ data class Direction(override val row: Int, override val col: Int): Vector<Int> 
     }
 }
 
+fun List<String>.charAt(position: Position):String = if (position.row in indices && position.col in get(position.row).indices) this[position.row][position.col].toString() else ""
+
+fun List<String>.positionsOf(s:String) = indices.flatMap { row -> this[row].indices.map{col -> Position(row, col) }}.filter{ charAt(it) == s}
+
 data class PositionD(override val row: Double, override val col: Double):Vector<Double> {
     operator fun plus(other: Vector<Double>) = PositionD(this.row + other.row, this.col + other.col)
     operator fun times(scalar: Int) = PositionD(this.row * scalar, this.col * scalar)
