3fdc4efcc21c606cacd8f5ae310f6d15bb697cad
Mike Neilens <michaelneilens@Michaels-Mac-mini.local>
2024-12-10 00:00:35 +0000

    advent day8

--- src/main/kotlin/day08/Problem.kt
+++ src/main/kotlin/day08/Problem.kt
@@ -1,9 +1,6 @@
 package day08
 
 import lib.Position
-import lib.PositionD
-import lib.Step
-import kotlin.math.abs
 
 fun partOne(data:List<String>) =
      parse(data).flatMap {(_, antennas) -> antennas.combinations()}.toSet()
@@ -38,20 +35,7 @@ fun partTwo(data:List<String>) =
         .flatMap{ antennas -> positionsOnLine(antennas.first(),antennas.last(),data.lastIndex, data.first().lastIndex)}
         .toSet().size
 
-fun positionsOnLine(a:Position, b:Position,maxRow: Int, maxCol: Int) =
-    (positionsOnLine(a, b, maxRow, maxCol, -1) + positionsOnLine(a, b, maxRow, maxCol, +1) + setOf(Position(a.row, a.col)))
-
-fun positionsOnLine(a:Position, b:Position, maxRow:Int, maxCol:Int, inc:Int):Set<Position> {
-    val step = reduceFraction( b.row - a.row, b.col - a.col )
-    var i = inc
-    val result = mutableSetOf<Position>()
-    while ((a + step * i).isInRange(maxRow, maxCol) ) {
-        result.add(a + step * i)
-        i+=inc
-    }
-    return result
+fun positionsOnLine(a:Position, b:Position, maxRow:Int, maxCol:Int):Set<Position> {
+    val step = Position( b.row - a.row, b.col - a.col)
+    return (minOf(-a.row,-a.col)..maxOf(maxRow - a.row, maxCol - a.col)).map{a + step * it}.filter{it.isInRange(maxRow, maxCol)}.toSet()
 }
-
-fun reduceFraction(x: Int, y: Int) = gcd(x,y).let{ gcd -> Position(x, y)}
-
-fun gcd(a: Int, b: Int): Int = if (b == 0)  a else gcd(b, a % b)
