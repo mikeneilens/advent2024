0e962cb9773698db81f1c9e235cf1e76e04169a4
Mike Neilens <michaelneilens@Michaels-Mac-mini.local>
2024-12-10 22:49:46 +0000

    advent day10

--- src/main/kotlin/day10/Problem.kt
+++ src/main/kotlin/day10/Problem.kt
@@ -1,9 +1,23 @@
 package day10
 
-fun partOne(data:List<String>): Int {
-    return 0
-}
+import lib.Direction
+import lib.Position
 
-fun partTwo(data:List<String>): Int {
-    return 0
-}
+fun partOne(data:List<String>) = data.trailHeads().sumOf{data.paths(it).distinct().size}
+
+fun List<String>.trailHeads() = indices.flatMap { row -> this[row].indices.map{col -> Position(row, col) }}.filter{ char(it) == "0"}
+
+fun List<String>.paths(currentPosition:Position):List<Position> =
+    if (char(currentPosition) == "9") listOf(currentPosition)
+    else nextPosition(currentPosition).flatMap { paths(it)}
+
+fun List<String>.nextPosition(position: Position) =
+    listOf(Direction.up, Direction.down, Direction.left, Direction.right).map{position + it}.filter{ newPosition ->
+        newPosition.isInRange(lastIndex,first().lastIndex) && char(newPosition).value() - char(position).value() == 1
+    }
+
+fun List<String>.char(position: Position):String = if (position.row in indices && position.col in get(position.row).indices) this[position.row][position.col].toString() else ""
+
+fun String.value() = toIntOrNull() ?: Int.MIN_VALUE
+
+fun partTwo(data:List<String>)= data.trailHeads().sumOf{data.paths(it).size}
--- src/test/kotlin/day10/ProblemTest.kt
+++ src/test/kotlin/day10/ProblemTest.kt
@@ -2,20 +2,51 @@ package day10
 
 import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
+import lib.Position
 
 class ProblemTest:WordSpec ({
     val sampleData = """
-        
+        89010123
+        78121874
+        87430965
+        96549874
+        45678903
+        32019012
+        01329801
+        10456732
     """.trimIndent().split("\n")
 
     "With part one" should {
-        "return 0" {
-            partOne(sampleData) shouldBe 0
+        "trail heads in sample data are" {
+            sampleData.trailHeads() shouldBe listOf(
+                Position(row=0, col=2),
+                Position(row=0, col=4),
+                Position(row=2, col=4),
+                Position(row=4, col=6),
+                Position(row=5, col=2),
+                Position(row=5, col=5),
+                Position(row=6, col=0),
+                Position(row=6, col=6),
+                Position(row=7, col=1)
+            )
+        }
+        "there are 5 trails from Position(0,2)" {
+            sampleData.paths(Position(0, 2)).distinct().size shouldBe 5
+        }
+        "part one with sample data gives" {
+            partOne(sampleData) shouldBe 36
+        }
+        "answer to part one is 531" {
+            partOne(puzzleInput) shouldBe 531
         }
     }
     "With part two" should {
-        "return 0" {
-            partTwo(sampleData) shouldBe 0
+        "part two with sample data gives 81" {
+            partTwo(sampleData) shouldBe 81
         }
+        "answer to part two is 1210" {
+            partTwo(puzzleInput) shouldBe 1210
+        }
+
     }
 })
