da73a74b72dc4cb185d18e5347773b69f0544b5a
Mike Neilens <michaelneilens@Michaels-Mac-mini.local>
2024-12-10 19:52:04 +0000

    advent day9

--- src/main/kotlin/day09/Problem.kt
+++ src/main/kotlin/day09/Problem.kt
@@ -1,9 +1,87 @@
 package day09
 
-fun partOne(data:List<String>): Int {
-    return 0
+sealed interface BlockType
+
+data class Block(val num:Int, val id:Int):BlockType
+object None:BlockType
+
+fun partOne(data:String): Long {
+    val input = parse(data)
+    return input.toMutableList<BlockType>().moveBlock().filter{it is Block}.checksum()
 }
 
-fun partTwo(data:List<String>): Int {
-    return 0
+fun List<BlockType>.checksum() = foldIndexed(0L) { i, result, block ->
+    if (block is Block) result + i * block.num
+    else result
 }
+
+fun MutableList<BlockType>.moveBlock():MutableList<BlockType> {
+    var start = 0
+    var end = lastIndex
+    while (start < end) {
+        if (this[start] is None) {
+            if (this[end] is None) {
+                end = this.nextEnd(end)
+            }
+            swap(start, end)
+            end--
+        }
+        start++
+    }
+    return this
+}
+fun MutableList<BlockType>.nextEnd(i:Int):Int = if (this[i] is None) nextEnd(i - 1) else i
+
+fun <E>MutableList<E>.swap(a:Int, b:Int) { this[a] = this[b].also { this[b] = this[a] }}
+
+fun parse(data:String):List<BlockType> =
+    data.flatMapIndexed { i, c ->
+        if (i % 2 == 0)
+            List(c.toString().toInt()) { Block(i / 2, i / 2) }
+        else
+            List(c.toString().toInt()) { None }
+    }
+
+sealed interface BigBlockType {
+    val size:Int
+}
+
+data class BigBlock(override val size:Int, val num:Int, val id:Int):BigBlockType
+data class BigNone(override val size:Int):BigBlockType
+
+fun partTwo(data:String): Long {
+    val input = parse2(data)
+    return input.toMutableList<BigBlockType>().moveBlock2().toBlocks().checksum()
+}
+
+fun List<BigBlockType>.toBlocks() = flatMap{block ->  List(block.size){if (block is BigBlock) Block(block.num,0) else None} }
+
+fun parse2(data:String):List<BigBlockType> =
+    data.mapIndexed { i, c ->
+        if (i % 2 == 0)
+            BigBlock(c.toString().toInt(),i / 2, i / 2)
+        else
+            BigNone(c.toString().toInt())
+    }
+
+fun MutableList<BigBlockType>.moveBlock2():MutableList<BigBlockType> {
+    var end = lastIndex
+    while (end > 0) {
+        val start = indexOfFirst{ it is BigNone && it.size >= this[end].size}
+        if (start >= 0 && start < end) {
+            val startSize = this[start].size
+            val endSize  = this[end].size
+            if (this[end].size < this[start].size) {
+                swap(start, end)
+                set(end, BigNone(endSize))
+                add(start + 1, BigNone(startSize - endSize))
+            } else {
+                swap(start, end)
+            }
+        }
+        end = nextEnd2(end - 1)
+    }
+    return this
+}
+
+fun MutableList<BigBlockType>.nextEnd2(i:Int):Int = if (this[i] is BigBlock ) i else nextEnd2(i - 1)
--- src/test/kotlin/day09/ProblemTest.kt
+++ src/test/kotlin/day09/ProblemTest.kt
@@ -5,17 +5,54 @@ import io.kotest.matchers.shouldBe
 
 class ProblemTest:WordSpec ({
     val sampleData = """
-        
-    """.trimIndent().split("\n")
+        2333133121414131402
+    """.trimIndent()
 
     "With part one" should {
-        "return 0" {
-            partOne(sampleData) shouldBe 0
+        "2333133121414131402 parses to 00...111...2...333.44.5555.6666.777.888899" {
+            parse("2333133121414131402").map{ if (it is Block) "${it.num}" else "." }.joinToString("")  shouldBe "00...111...2...333.44.5555.6666.777.888899"
+        }
+        "00...111...2...333.44.5555.6666.777.888899 after moving blocks is 0099811188827773336446555566" {
+            val input = parse(sampleData)
+            println(input.toMutableList().moveBlock())
+            input.toMutableList().moveBlock().map{ if (it is Block) "${it.num}" else "." }.joinToString("") shouldBe "0099811188827773336446555566.............."
+        }
+        "checksum of 0099811188827773336446555566 is 1928 " {
+            val input = parse(sampleData)
+            input.toMutableList().moveBlock().filter { it is Block }.checksum() shouldBe 1928
+        }
+        "part one using sample data is 1928" {
+            partOne(sampleData) shouldBe 1928
+        }
+        "answer to part one is " {
+            //90192861055 too low
+            partOne(puzzleInput) shouldBe 1928
         }
     }
     "With part two" should {
-        "return 0" {
-            partTwo(sampleData) shouldBe 0
+        "parse sample data into BigBlockType" {
+            parse2(sampleData) shouldBe listOf(
+                BigBlock(size=2, num=0, id=0),
+                BigNone(size=3), BigBlock(size=3, num=1, id=1),
+                BigNone(size=3), BigBlock(size=1, num=2, id=2),
+                BigNone(size=3), BigBlock(size=3, num=3, id=3),
+                BigNone(size=1), BigBlock(size=2, num=4, id=4),
+                BigNone(size=1), BigBlock(size=4, num=5, id=5),
+                BigNone(size=1), BigBlock(size=4, num=6, id=6),
+                BigNone(size=1), BigBlock(size=3, num=7, id=7),
+                BigNone(size=1), BigBlock(size=4, num=8, id=8),
+                BigNone(size=0), BigBlock(size=2, num=9, id=9)
+            )
+        }
+        "00...111...2...333.44.5555.6666.777.888899 after moving blocks is 00992111777.44.333....5555.6666.....8888.." {
+            parse2(sampleData).toMutableList().moveBlock2()
+                .map{block -> List(block.size){if (block is BigBlock) "${block.num}" else "."  }.joinToString("")  }.joinToString("") shouldBe "00992111777.44.333....5555.6666.....8888.."
+        }
+        "part two using sample data is 2858" {
+            partTwo(sampleData) shouldBe 2858
+        }
+        "answer to part two is " {
+            partTwo(puzzleInput) shouldBe 6347435485773
         }
     }
 })
