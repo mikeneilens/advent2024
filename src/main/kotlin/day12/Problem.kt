952eae06d017dc71dfb8388902b97c5b925506e4
Mike Neilens <michaelneilens@Michaels-Mac-mini.local>
2024-12-13 09:24:55 +0000

    advent day12

--- src/main/kotlin/day12/Problem.kt
+++ src/main/kotlin/day12/Problem.kt
@@ -30,6 +30,31 @@ fun List<String>.plotBoundarySize(plot: Set<Position>) =
 
 fun Position.adjacentPositions() = setOf(plus(Direction.up), plus(Direction.right), plus(Direction.down), plus(Direction.left))
 
-fun partTwo(data:List<String>): Int {
-    return 0
+fun partTwo(data:List<String>): Int =
+    data.getPlotsAndSides().sumOf { it.first * it.second }
+
+fun List<String>.getPlotsAndSides():List<Pair<Int, Int>> {
+    val visited = mutableSetOf<Position>()
+    val plots = allPositions().mapNotNull{position ->
+        if (position !in visited) {
+            getPlot(charAt(position), setOf(position),visited)
+        } else null
+    }
+    return plots.map{ Pair(it.size, plotSides(it) ) }
 }
+
+fun List<String>.plotSides(plot: Set<Position>):Int {
+    val rows = plot.rows()
+    val columns = plot.columns()
+    return rows.sumOf{row -> boundary(plot, row, Direction.up)} + rows.sumOf{row -> boundary(plot, row, Direction.down)} +
+            columns.sumOf{column -> boundary(plot, column, Direction.left)} + columns.sumOf{column -> boundary(plot, column, Direction.right)}
+}
+
+fun Set<Position>.columns() =  (minOf { it.col }..maxOf { it.col }).map{col ->  (minOf { it.row }..maxOf { it.row }).map{row-> Position(row, col)}}
+fun Set<Position>.rows() =  (minOf { it.row }..maxOf { it.row }).map{row ->  (minOf { it.col }..maxOf { it.col }).map{col-> Position(row, col)}}
+
+fun List<String>.boundary(plot:Set<Position>, positions:List<Position>,direction:Direction) = positions.fold(Pair(0, false)){ output, position ->
+    if (position in plot &&  charAt(position + direction) != charAt(position))
+        if (output.second) Pair(output.first, true) else Pair(output.first + 1, true)
+    else Pair(output.first, false)
+}.first
