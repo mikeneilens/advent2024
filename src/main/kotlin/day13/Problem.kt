9bc4b99645150ec1e2ec69c087845c363c50685e
Mike Neilens <michaelneilens@Michaels-Mac-mini.local>
2024-12-13 19:15:34 +0000

    advent day13

--- src/main/kotlin/day13/Problem.kt
+++ src/main/kotlin/day13/Problem.kt
@@ -1,9 +1,29 @@
 package day13
 
-fun partOne(data:List<String>): Int {
-    return 0
+fun partOne(data:List<List<String>>): Int {
+    return data.parse().minCostOfPresses().sumOf{ it.min() }
 }
 
-fun partTwo(data:List<String>): Int {
+data class Game(val buttonAX:Int, val buttonAY:Int, val buttonBX:Int, val buttonBY:Int, val prizeX:Int, val prizeY:Int) {
+    fun costOfPresses(aPresses:Int):Int? {
+        val bPresses = ((prizeX + prizeY) - aPresses * (buttonAX + buttonAY)) / (buttonBX + buttonBY)
+        println("$this $aPresses $bPresses" )
+        if (aPresses <= 100 && bPresses <= 100 && (buttonAX * aPresses + buttonBX * bPresses) == prizeX && (buttonAY * aPresses + buttonBY * bPresses) == prizeY)
+            return aPresses * 3 + bPresses
+        else return null
+    }
+}
+
+fun List<Game>.minCostOfPresses() = map{ game ->  (1..100).mapNotNull{n -> game.costOfPresses(n)}}.filter { it.isNotEmpty() }
+
+fun List<List<String>>.parse():List<Game> =
+    map{ s-> Game( s[0].toButtonX(),s[0].toButtonY(), s[1].toButtonX(),s[1].toButtonY(), s[2].toPrizeX(),s[2].toPrizeY()) }
+
+fun String.toButtonX() = split("X")[1].split(",")[0].toInt()
+fun String.toButtonY() = split("Y")[1].toInt()
+fun String.toPrizeX() = split("X=")[1].split(",")[0].toInt()
+fun String.toPrizeY() = split("Y=")[1].toInt()
+
+fun partTwo(data:List<List<String>>): Int {
     return 0
 }
--- src/test/kotlin/day13/ProblemTest.kt
+++ src/test/kotlin/day13/ProblemTest.kt
@@ -5,12 +5,43 @@ import io.kotest.matchers.shouldBe
 
 class ProblemTest:WordSpec ({
     val sampleData = """
+        Button A: X+94, Y+34
+        Button B: X+22, Y+67
+        Prize: X=8400, Y=5400
         
-    """.trimIndent().split("\n")
+        Button A: X+26, Y+66
+        Button B: X+67, Y+21
+        Prize: X=12748, Y=12176
+        
+        Button A: X+17, Y+86
+        Button B: X+84, Y+37
+        Prize: X=7870, Y=6450
+        
+        Button A: X+69, Y+23
+        Button B: X+27, Y+71
+        Prize: X=18641, Y=10279
+    """.trimIndent().split("\n").chunked(4)
 
     "With part one" should {
-        "return 0" {
-            partOne(sampleData) shouldBe 0
+        "parse sampleData " {
+            sampleData.parse() shouldBe listOf(
+                Game(buttonAX=94, buttonAY=34, buttonBX=22, buttonBY=67, prizeX=8400, prizeY=5400),
+                Game(buttonAX=26, buttonAY=66, buttonBX=67, buttonBY=21, prizeX=12748, prizeY=12176),
+                Game(buttonAX=17, buttonAY=86, buttonBX=84, buttonBY=37, prizeX=7870, prizeY=6450),
+                Game(buttonAX=69, buttonAY=23, buttonBX=27, buttonBY=71, prizeX=18641, prizeY=10279)
+            )
+        }
+        "with first game, pushing the a button 80 times gives an answer of 280" {
+            sampleData.parse().first().costOfPresses(80) shouldBe 280
+        }
+        "min cost of all games in sample data" {
+            sampleData.parse().minCostOfPresses() shouldBe listOf(listOf(280),listOf(200))
+        }
+        "answer to part one using sample data is 480" {
+            partOne(sampleData) shouldBe 480
+        }
+        "answer to part one is 39478 "{
+            partOne(puzzleInput) shouldBe 39478
         }
     }
     "With part two" should {
