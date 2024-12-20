5c1948e7728e83d5ebecbdccd8af489467497b4f
Mike Neilens <michaelneilens@Michaels-Mac-mini.local>
2024-12-20 15:16:31 +0000

    day 19

--- src/main/kotlin/day19/Problem.kt
+++ src/main/kotlin/day19/Problem.kt
@@ -3,24 +3,24 @@ package day19
 fun partOne(patterns:String, designs:List<String>): Int {
     val patterns = patterns.parsePatterns()
     return designs.filter{ design ->
-        matchingPatterns(design, patterns, listOf()).isNotEmpty()
+        matchingPatterns(design, patterns).isNotEmpty()
     }.size
 }
 
 fun String.parsePatterns() = split(", ").toSet()
 
-fun matchingPatterns(design:String, patterns:Set<String>, output:List<String>, memo:MutableMap<String,List<List<Boolean>>> = mutableMapOf()):List<List<Boolean>> {
+fun matchingPatterns(design:String, patterns:Set<String>, memo:MutableMap<String,List<List<Boolean>>> = mutableMapOf()):List<List<Boolean>> {
     if (design.isEmpty()) return listOf(listOf(true))
     else if (design in memo) return memo.getValue(design)
     else return  patterns.filter{pattern -> design.startsWith(pattern)}.flatMap { pattern ->
-        matchingPatterns(design.removePrefix(pattern), patterns, output + pattern, memo).also{memo[design] = it}
+        matchingPatterns(design.removePrefix(pattern), patterns, memo).also{memo[design] = it}
     }
 }
 
 fun partTwo(patterns:String, designs:List<String>): Int {
     val patterns = patterns.parsePatterns()
-    designs.forEach{design -> println( "$design ${matchingPatterns(design, patterns, listOf())}")}
+    designs.forEach{design -> println( "$design ${matchingPatterns(design, patterns)}")}
     return designs.sumOf{ design ->
-        matchingPatterns(design, patterns, listOf()).count()
+        matchingPatterns(design, patterns).count()
     }
 }
