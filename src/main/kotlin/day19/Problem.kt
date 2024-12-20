39b1d0a43f9accfc94e783d789fb596321fb56b2
Mike Neilens <michaelneilens@Michaels-Mac-mini.local>
2024-12-20 15:15:00 +0000

    day 19

--- src/main/kotlin/day19/Problem.kt
+++ src/main/kotlin/day19/Problem.kt
@@ -1,9 +1,26 @@
 package day19
 
-fun partOne(data:List<String>): Int {
-    return 0
+fun partOne(patterns:String, designs:List<String>): Int {
+    val patterns = patterns.parsePatterns()
+    return designs.filter{ design ->
+        matchingPatterns(design, patterns, listOf()).isNotEmpty()
+    }.size
 }
 
-fun partTwo(data:List<String>): Int {
-    return 0
+fun String.parsePatterns() = split(", ").toSet()
+
+fun matchingPatterns(design:String, patterns:Set<String>, output:List<String>, memo:MutableMap<String,List<List<Boolean>>> = mutableMapOf()):List<List<Boolean>> {
+    if (design.isEmpty()) return listOf(listOf(true))
+    else if (design in memo) return memo.getValue(design)
+    else return  patterns.filter{pattern -> design.startsWith(pattern)}.flatMap { pattern ->
+        matchingPatterns(design.removePrefix(pattern), patterns, output + pattern, memo).also{memo[design] = it}
+    }
+}
+
+fun partTwo(patterns:String, designs:List<String>): Int {
+    val patterns = patterns.parsePatterns()
+    designs.forEach{design -> println( "$design ${matchingPatterns(design, patterns, listOf())}")}
+    return designs.sumOf{ design ->
+        matchingPatterns(design, patterns, listOf()).count()
+    }
 }
--- src/test/kotlin/day19/ProblemTest.kt
+++ src/test/kotlin/day19/ProblemTest.kt
@@ -4,18 +4,32 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class ProblemTest:WordSpec ({
-    val sampleData = """
-        
+    val sampleDataPatterns = "r, wr, b, g, bwu, rb, gb, br"
+    val sampleDataDesigns = """
+        brwrr
+        bggr
+        gbbr
+        rrbgbr
+        ubwu
+        bwurrg
+        brgr
+        bbrgwb
     """.trimIndent().split("\n")
 
     "With part one" should {
-        "return 0" {
-            partOne(sampleData) shouldBe 0
+        "part one using sample data is 6" {
+            partOne(sampleDataPatterns, sampleDataDesigns) shouldBe 6
+        }
+        "answer to part one is 327" {
+            partOne(puzzleInputPatterns, puzzleInputDesigns) shouldBe 327
         }
     }
     "With part two" should {
-        "return 0" {
-            partTwo(sampleData) shouldBe 0
+        "part two using sample data is 16" {
+            partTwo(sampleDataPatterns, sampleDataDesigns) shouldBe 16
+        }
+        "answer to part two is" {
+            partTwo(puzzleInputPatterns, puzzleInputDesigns) shouldBe 0
         }
     }
 })
