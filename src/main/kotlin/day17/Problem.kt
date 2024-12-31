f2f222fda0a936e198f689e389908ad9277789de
Mike Neilens <michaelneilens@Michaels-Mac-mini.local>
2024-12-31 21:54:12 +0000

    day17

--- src/main/kotlin/day17/Problem.kt
+++ src/main/kotlin/day17/Problem.kt
@@ -1,7 +1,47 @@
 package day17
 
-fun partOne(data:List<String>): Int {
-    return 0
+import kotlin.math.pow
+
+data class Status(val a:Long, val b:Long, val c:Long, val ip:Int, val output:String) {
+
+    val comboMap:Map<Int, Long> = mapOf(4 to a, 5 to b, 6 to c )
+
+    fun shouldHalt() = ip > instructions.lastIndex
+
+    fun getCombo(combo:Int) = comboMap.getOrDefault(combo, combo).toInt()
+
+    fun adv(operand:Int) = copy(a = (a/2.0.pow(getCombo(operand)).toLong()), ip = ip + 2)
+
+    fun bxl(operand: Int) = copy (b = b.xor(operand.toLong()), ip = ip + 2)
+
+    fun bst(operand: Int) = copy (b = (getCombo(operand) % 8).toLong(), ip = ip + 2)
+
+    fun jnz(operand: Int) = if (a == 0L) copy(ip = ip + 2 ) else copy (ip = operand)
+
+    fun bxc(operand: Int) = copy(b = b.xor(c), ip = ip + 2)
+
+    fun out(operand: Int) = copy(output = (output + ",${getCombo(operand) % 8}").removePrefix(","), ip = ip + 2)
+
+    fun bdv(operand:Int) = copy(b = (a/2.0.pow(getCombo(operand)).toLong()), ip = ip + 2)
+
+    fun cdv(operand:Int) = copy(c = (a/2.0.pow(getCombo(operand)).toLong()), ip = ip + 2)
+
+    fun process() = Status.operatorMap.getValue(instructions[ip])(instructions[ip + 1])
+
+    companion object {
+        val operatorMap:Map<Int, Status.(Int)->Status> = mapOf(0 to Status::adv, 1 to Status::bxl, 2 to Status::bst, 3 to Status::jnz,
+            4 to Status::bxc, 5 to Status::out, 6 to Status::bdv, 7 to Status::cdv)
+        var instructions:List<Int> = listOf()
+    }
+}
+
+fun partOne(data:Status, program:List<Int>): Status {
+    var status = data
+    Status.instructions = program
+    while (!status.shouldHalt()) {
+        status = status.process()
+    }
+    return status
 }
 
 fun partTwo(data:List<String>): Int {
--- src/test/kotlin/day17/ProblemTest.kt
+++ src/test/kotlin/day17/ProblemTest.kt
@@ -9,8 +9,40 @@ class ProblemTest:WordSpec ({
     """.trimIndent().split("\n")
 
     "With part one" should {
+        "If register C contains 9, the program 2,6 would set register B to 1." {
+            Status.instructions = listOf(2,6)
+            val status = Status(a = 0, b = 0, c = 9, ip = 0, output = "")
+            val newStatus = status.process()
+            newStatus.b shouldBe 1
+        }
+        "If register A contains 10, the program 5,0,5,1,5,4 would output 0,1,2" {
+            Status.instructions = listOf(5,0,5,1,5,4)
+            val initialStatus = Status(a = 10, b = 0, c = 0 , ip = 0, output = "")
+            val status1 = initialStatus.process()
+            val status2 = status1.process()
+            val status3 = status2.process()
+            status3.output shouldBe "0,1,2"
+        }
+        "If register A contains 2024, the program 0,1,5,4,3,0 would output 4,2,5,6,7,7,7,7,3,1,0 and leave 0 in register A." {
+
+            val initialStatus = Status(a = 2024, b = 0, c = 0 , ip = 0, output = "")
+            val finalStatus = partOne(initialStatus, listOf(0,1,5,4,3,0))
+            finalStatus.output shouldBe "4,2,5,6,7,7,7,7,3,1,0"
+            finalStatus.a shouldBe 0
+        }
+        "If register B contains 29, the program 1,7 would set register B to 26." {
+            val initialStatus = Status(a = 0, b = 29, c = 0 , ip = 0, output = "")
+            val finalStatus = partOne(initialStatus, listOf(1,7))
+            finalStatus.b shouldBe 26
+        }
+        "If register B contains 2024 and register C contains 43690, the program 4,0 would set register B to 44354." {
+            val initialStatus = Status(a = 0, b = 2024, c = 43690 , ip = 0, output = "")
+            val finalStatus = partOne(initialStatus, listOf(4,0))
+            finalStatus.b shouldBe 44354
+        }
+
         "return 0" {
-            partOne(sampleData) shouldBe 0
+            //partOne(sampleData) shouldBe 0
         }
     }
     "With part two" should {
