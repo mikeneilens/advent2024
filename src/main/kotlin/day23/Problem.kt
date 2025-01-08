3f432a58b497efe022a522cba74178b99c7bd1ef
Mike Neilens <michaelneilens@Michaels-Mac-mini.local>
2025-01-08 19:49:54 +0000

    day23

--- src/main/kotlin/day23/Problem.kt
+++ src/main/kotlin/day23/Problem.kt
@@ -2,11 +2,9 @@ package day23
 
 fun partOne(data:List<String>): Int {
     val keyMap = data.createMap()
-    return getTrios(keyMap).count{ set -> set.any { s:String -> s.startsWith ("t") } }
+    return keyMap.flatMap { combinations(it.value + it.key).filter { combo -> combo.size == 3 && combo.allConnected(keyMap) } }
+        .distinct().count{ set -> set.any { s:String -> s.startsWith ("t") } }
 }
-
-fun getTrios(keyMap: Map<String, Set<String>>) =
-    keyMap.flatMap { keyMap.matchesWith(it.key) }.toSet()
 
 fun List<String>.createMap():Map<String, Set<String>> {
     val map = mutableMapOf<String, Set<String>>()
@@ -17,13 +15,6 @@ fun List<String>.createMap():Map<String, Set<String>> {
 fun MutableMap<String, Set<String>>.updateWithPair(a:String, b:String) {
     get(a)?.let{ set(a, it + b)} ?: set(a, setOf(b))
     get(b)?.let{ set(b, it + a)} ?: set(b, setOf(a))
-}
-
-fun Map<String, Set<String>>.matchesWith(s:String):Set<Set<String>> {
-    val candidates = this[s] ?: setOf()
-    return  candidates.flatMap{
-        candidate -> this.getValue(candidate).map{third -> if (s in this.getValue(third)) listOf(s, candidate, third) else listOf()}
-    }.map{it.toSet()}.filter{it.size > 2}.toSet()
 }
 
 fun partTwo(data:List<String>): String {
--- src/test/kotlin/day23/ProblemTest.kt
+++ src/test/kotlin/day23/ProblemTest.kt
@@ -54,45 +54,6 @@ td-yn
             map["qp"] shouldBe setOf("kh", "ub", "td", "wh")
             setOf("tc", "qp", "ub", "ta") intersect setOf("kh", "ub", "td", "wh") shouldBe setOf("ub")
         }
-        "get matches with kh" {
-            val map = sampleData.createMap()
-            map.matchesWith("kh") shouldBe setOf(setOf("ub", "qp", "kh"))
-        }
-        "get matches with aq" {
-            val map = sampleData.createMap()
-            map.matchesWith("aq") shouldBe setOf(setOf("aq", "yn", "cg"),setOf("aq", "vc", "wq"))
-        }
-        "get matches with de" {
-            val map = sampleData.createMap()
-            map.matchesWith("de") shouldBe setOf(setOf("de", "co", "ka"),setOf("de", "co", "ta"), setOf("de", "ta", "ka"))
-        }
-        "trios for the sample data" {
-            val map = sampleData.createMap()
-            getTrios(map) shouldBe setOf(
-                setOf("aq","cg","yn"),
-                setOf("aq","vc","wq"),
-                setOf("co","de","ka"),
-                setOf("co","de","ta"),
-                setOf("co","ka","ta"),
-                setOf("de","ka","ta"),
-                setOf("kh","qp","ub"),
-                setOf("qp","td","wh"),
-                setOf("tb","vc","wq"),
-                setOf("tc","td","wh"),
-                setOf("td","wh","yn"),
-                setOf("ub","vc","wq"),
-            )
-        }
-        "trios for puzzlie input" {
-            val map = puzzleInput.createMap()
-            val trios = getTrios(map)
-            trios.forEach{ set ->
-                val strings = set.toList()
-                if (strings[0] !in map.getValue(strings[1]) || strings[0] !in map.getValue(strings[2])) println("error with set $set")
-                if (strings[1] !in map.getValue(strings[0]) || strings[1] !in map.getValue(strings[2])) println("error with set $set")
-                if (strings[2] !in map.getValue(strings[0]) || strings[2] !in map.getValue(strings[1])) println("error with set $set")
-            }
-        }
         "answer with sample data is 7" {
             partOne(sampleData) shouldBe 7
         }
